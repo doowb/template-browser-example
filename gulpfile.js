44c842fee908e7bb0386b9f511b3e508e8294810
Brian Woodward <brian.woodward@gmail.com>
2015-01-21 17:51:16 -0500

    setting up browserify in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,18 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('example', function () {
-  var bundler = browserify('./src/app.js', { debug: true });
+  var bundler = browserify('./src/app.js', { debug: true })
+    .require('./package.json', { expose: '/package.json' })
+    .exclude('coffee-script')
+    .exclude('toml');
 
   var bundle = function () {
     return bundler
       .bundle()
       .pipe(source('app.js'))
       .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./js/'));
   };
 
